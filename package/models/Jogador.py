728c81aa859a21e3755ac8dabd5a2280443399b3
Rodrigoatila09 <154346800+Rodrigoatila09@users.noreply.github.com>
2025-05-11 18:23:29 -0300

    Alterações em dados do usuario

--- package/models/Jogador.py
+++ package/models/Jogador.py
@@ -1,6 +1,22 @@
 class Jogador():
 
-    def __init__(self, nome, cpf, dinheiro):
+
+    def __init__(self, nome="", cpf=""):
         self.nome = nome
         self.cpf = cpf
-        self.dinheiro = dinheiro
+        self.dinheiro = 100
+        self.saldo = 0
+
+    def nomeJogador(self):
+        inputNome = input("Digite seu nome:\n")
+        self.nome = inputNome
+        return self.nome
+    
+    def cpfJogador(self):
+        cpfJogador = int(input("Digite o número de seu CPF:\n"))
+        self.cpf = cpfJogador
+        return self.cpf
+
+
+
+    
